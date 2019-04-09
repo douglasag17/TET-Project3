335d6836ea282583d3e4bc106a581903a6e477f1
Douglas Ardila Garcés <dag1702@hotmail.com>
2019-04-08 19:18:20 -0500

    Update procesarArchivos.py

--- procesarArchivos.py
+++ procesarArchivos.py
@@ -11,6 +11,7 @@ frame = pd.DataFrame()
 i = 1
 for file_ in allFiles:
     df = pd.read_csv(file_,index_col=None, header=0, usecols=columns)
+    df['content'] = df['content'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopWords)]))
     df['content'] = df['content'].str.lower()
     df['content'] = df['content'].str.replace(',', ' ')
     df['content'] = df['content'].str.replace('.', ' ')
@@ -28,7 +29,6 @@ for file_ in allFiles:
     df['content'] = df['content'].str.replace(')', ' ')
     df['content'] = df['content'].str.replace('“', ' ')
     df['content'] = df['content'].str.replace('‘', ' ')
-    df['content'] = df['content'].str.replace('\t', ' ')    
-    df['content'] = df['content'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopWords)]))
+    df['content'] = df['content'].str.replace('\t', ' ')
     df.to_csv("Particles"+str(i)+".csv", sep='\t', index=True, header=True)
     i+=1
