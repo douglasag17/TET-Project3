a1e08dded8723d05ad67547d9126ec33b4873663
Felipe Macías H <maciasfelipe8006@gmail.com>
2019-04-28 15:29:02 -0500

    Update cleaning_articles.py

--- databricks/cleaning_articles.py
+++ databricks/cleaning_articles.py
@@ -2,17 +2,18 @@
 from pyspark.sql.functions import regexp_replace, concat_ws
 from pyspark.ml.feature import StopWordsRemover
 from pyspark.ml.feature import Tokenizer, RegexTokenizer
-
+import pyspark.sql.functions as f
+from collections import defaultdict
 # File location and type
-file_path = "dbfs:///FileStore/tables/articles*.csv"
+file_path = "dbfs:///FileStore/tables/*.csv"
 df = spark.read.csv(file_path, header="true", inferSchema="true").select("id", "title", "content")
+df = df.filter(df.content.isNotNull())#Removing null values
 df = df.withColumn('content', regexp_replace('content', '[^0-9a-zA-Z]+', ' '))#remove special characteres
 df = df.withColumn('content', regexp_replace('content', '(?:^| )\w(?:$| )', ' '))#remove single words
-
-tokenizer = Tokenizer(inputCol="content", outputCol="words")
+#Removing stopwords
+tokenizer = RegexTokenizer(inputCol="content", outputCol="words", pattern="\\W")
 tokenized = tokenizer.transform(df).select('id', 'title' ,'words')
-
 remover = StopWordsRemover(inputCol="words", outputCol="filtered")
 cleanedDataFrame = remover.transform(tokenized).select('id', 'title' ,'filtered')
 cleanedDataFrame = cleanedDataFrame.withColumn('filtered', concat_ws(' ', cleanedDataFrame.filtered))
-display(cleanedDataFrame)
+frame = cleanedDataFrame.toPandas()
