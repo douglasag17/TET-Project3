abbda075ba3e9933a2172f8c2f5414f92eccc877
avendanor <31228279+avendanor@users.noreply.github.com>
2019-04-28 19:49:29 -0500

    Update grouping_articles.py

--- databricks/grouping_articles.py
+++ databricks/grouping_articles.py
@@ -17,13 +17,13 @@ title = cleanedDataFrame.filter(cleanedDataFrame.id == id_).collect()[0][1]
 wordsN = grouping.get(id_)
 dictOfWords = { i[0] : i[1] for i in wordsN }
 for item in grouping.items():
-  cont=0
-  for word in item[1]:
-    if word[0] in dictOfWords:
-      cont += word[1]+dictOfWords[word[0]]
-  news[item[0]]=cont
+    cont=0
+    for word in item[1]:
+        if word[0] in dictOfWords:
+            cont += word[1]+dictOfWords[word[0]]
+    news[item[0]]=cont
 news = sorted(news.items(),key=lambda kv: kv[1], reverse = True)[:10]
 ids_ = []
 for i in news:
-  ids_.append(i[0])
+    ids_.append(i[0])
 print(id_, title, ids_)
